ddeb7725bd5d68b342a3007273c2dd1194b98a03
danielkahn <danielkahn8@gmail.com>
2015-08-10 15:02:48 -0700

    post controller, show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+	#user has many posts and only that user can delete post
+	has_many :posts, dependent: :destroy
+	has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,15 @@ get '/signup', to: 'user#new'
 get '/profile', to: 'user#show'
 resources :users, only: [:create]
 
+# session routes
+get '/login', to: 'sessions#new'
+get '/logout', to: 'sessions#destroy'
+#post '/sessions', to: 'sessions#create'
+resources :sessions, only: [:create]
+
+# posts routes
+resources :posts, except: [:index]
+
 root 'posts#index'
+
 end
