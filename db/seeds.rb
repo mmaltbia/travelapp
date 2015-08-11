72b774108419ff94e6d48d2a37dec952bd9b8f60
Michelle Maltbia <maltbia.michelle@gmail.com>
2015-08-11 08:14:38 -0700

    Adds nav bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+posts = Post.create([
+	{author:"Michelle M.", title:"Sunny California", body: "California, a western U.S. state, stretches from the Mexican border along the Pacific for nearly 900 miles. It's known for its dramatic terrain encompassing cliff-lined beaches, redwood forest, the Sierra Nevada Mountains, Central Valley farmland and the arid Mojave Desert. Its cities include sprawling Los Angeles, seat of the Hollywood entertainment industry, and hilly San Francisco, home to the Golden Gate Bridge."}
+])
